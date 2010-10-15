7f7d49c686cd63ee33dc4e8b5f2794e97ac36a0d
Johan Andersson <johan.rejeep@gmail.com>
2010-10-15 15:25:41 +0200

    Added Ecukes files.

---
+++ features/support/env.el
@@ -0,0 +1,13 @@
+(let* ((current-directory (file-name-directory load-file-name))
+       (features-directory (expand-file-name ".." current-directory))
+       (project-directory (expand-file-name ".." features-directory)))
+  (setq enclose-root-path project-directory)
+  (setq enclose-util-path (expand-file-name "util" project-directory)))
+
+(add-to-list 'load-path enclose-root-path)
+(add-to-list 'load-path (expand-file-name "espuds" enclose-util-path))
+(add-to-list 'load-path (expand-file-name "emacs-lisp" (expand-file-name "lisp" (expand-file-name "ert" enclose-util-path))))
+
+(require 'enclose)
+(require 'espuds)
+(require 'ert)
