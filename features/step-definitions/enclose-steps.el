b1e8ef1fecd9ed9da10d9f23b6dacf10f68f25b7
Johan Andersson <johan.rejeep@gmail.com>
2010-10-18 21:25:07 +0200

    Moving around stuff in steps file.

--- features/step-definitions/enclose-steps.el
+++ features/step-definitions/enclose-steps.el
@@ -6,51 +6,6 @@
        (lambda ()
          (setq enclose-remove-pair nil)))
 
-;; TODO: Submit to Espuds
-(Then "^the cursor should be after \"\\(.+\\)\"$"
-      (lambda (right)
-        (should (looking-at (regexp-quote right)))))
-
-;; TODO: Submit to Espuds
-(Then "^the cursor should be before \"\\(.+\\)\"$"
-      (lambda (left)
-        (should (looking-back (regexp-quote left)))))
-
-
-;; TODO: Submit to Espuds
-(Then "^the cursor should be between \"\\(.+\\)\" and \"\\(.+\\)\"$"
-      (lambda (left right)
-        (should
-         (and
-          (looking-back (regexp-quote left))
-          (looking-at (regexp-quote right))))))
-
-;; TODO: Submit to Espuds
-(When "^I place the cursor between \"\\(.+\\)\" and \"\\(.+\\)\"$"
-      (lambda (left right)
-        (goto-char (point-min))
-        (should (search-forward (concat left right) nil t))
-        (backward-char (length right))))
-
-;; TODO: Submit to Espuds
-(When "^I move the cursor$"
-      (lambda ()
-        (should (> (buffer-size) 0))
-        (save-excursion
-          (call-interactively
-           (if (eobp) 'backward-char 'forward-char)))))
-
-;; TODO: Submit to Espuds
-(When "^I go to beginning of buffer$" 'beginning-of-buffer)
-
-;; TODO: Submit to Espuds
-(When "^I go to end of buffer$" 'end-of-buffer)
-
-;; TODO: Submit to Espuds?
-(Then "I should not see anything"
-      (lambda ()
-        (should (equal (buffer-size) 0))))
-
 (Given "^I add encloser \"\\(.\\)/\\(.\\)\"$"
        (lambda (left right)
          (enclose-add-encloser left right)))
@@ -60,5 +15,42 @@
          (enclose-remove-encloser left)))
 
 (When "^I enable the global mode$"
-       (lambda ()
-         (enclose-global-mode 1)))
+      (lambda ()
+        (enclose-global-mode 1)))
+
+;; TODO: Submit below to Espuds
+
+(Then "^the cursor should be after \"\\(.+\\)\"$"
+      (lambda (right)
+        (should (looking-at (regexp-quote right)))))
+
+(Then "^the cursor should be before \"\\(.+\\)\"$"
+      (lambda (left)
+        (should (looking-back (regexp-quote left)))))
+
+
+(Then "^the cursor should be between \"\\(.+\\)\" and \"\\(.+\\)\"$"
+      (lambda (left right)
+        (should
+         (and
+          (looking-back (regexp-quote left))
+          (looking-at (regexp-quote right))))))
+
+(When "^I place the cursor between \"\\(.+\\)\" and \"\\(.+\\)\"$"
+      (lambda (left right)
+        (goto-char (point-min))
+        (should (search-forward (concat left right) nil t))
+        (backward-char (length right))))
+
+(When "^I go to beginning of buffer$" 'beginning-of-buffer)
+
+(When "^I go to end of buffer$" 'end-of-buffer)
+
+(Then "I should not see anything"
+      (lambda ()
+        (should (equal (buffer-size) 0))))
+
+(When "^I turn on \\(.+\\)$"
+      (lambda (mode)
+        (let ((v (vconcat [?\C-u 1 ?\M-x] (string-to-vector mode))))
+          (execute-kbd-macro v))))
