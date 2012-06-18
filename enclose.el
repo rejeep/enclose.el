61c44bdd5167cc1c50ab552116661582693055ea
Johan Andersson <johan.rejeep@gmail.com>
2012-06-18 21:40:16 +0200

    Update copyright years.

--- enclose.el
+++ enclose.el
@@ -1,6 +1,6 @@
 ;;; enclose.el --- Enclose cursor within punctuation pairs
 
-;; Copyright (C) 2010 Johan Andersson
+;; Copyright (C) 2010-2012 Johan Andersson
 
 ;; Author: Johan Andersson <johan.rejeep@gmail.com>
 ;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
